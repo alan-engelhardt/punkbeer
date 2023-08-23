ce91eb479cf2ea249d91cd2e15e71d92636ebda5
Alan <ale@kea.dk>
2023-08-23 12:24:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+fetch("https://api.punkapi.com/v2/beers")
+  .then((res) => res.json())
+  .then((data) => vis(data));
+
+const temp = document.querySelector("template").content;
+const gode = document.querySelector("#gode");
+const onde = document.querySelector("#onde");
+
+function vis(data) {
+  data.forEach((beer) => {
+    const klon = temp.cloneNode(true);
+    klon.querySelector("h3").textContent = beer.name;
+    klon.querySelector("img").src = beer.image_url;
+    if (beer.method.twist) {
+      klon.querySelector("article").classList.add("twisted");
+      klon.querySelector("details span").textContent = beer.method.twist;
+    }
+    if (beer.srm > 20) {
+      dark.appendChild(klon);
+    } else {
+      light.appendChild(klon);
+    }
+  });
+}
