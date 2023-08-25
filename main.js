324dfb1903850c04657e5ef6720f7a1ca3536742
Alan <ale@kea.dk>
2023-08-25 09:40:26 +0200

    stærk label

--- main.js
+++ main.js
@@ -10,10 +10,14 @@ function vis(data) {
   data.forEach((beer) => {
     const klon = temp.cloneNode(true);
     klon.querySelector("h3").textContent = beer.name;
+    klon.querySelector("p span").textContent = beer.abv;
     klon.querySelector("img").src = beer.image_url;
     if (beer.method.twist) {
       klon.querySelector("article").classList.add("twisted");
       klon.querySelector("details span").textContent = beer.method.twist;
+    }
+    if (beer.abv > 5.5) {
+      klon.querySelector("article").classList.add("strong");
     }
     if (beer.srm > 20) {
       dark.appendChild(klon);
