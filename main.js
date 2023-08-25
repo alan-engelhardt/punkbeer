49aa45fe49ad31a0ecc5cfcfc7127b12362d60ce
Alan <ale@kea.dk>
2023-08-25 09:23:37 +0200

    vis alt

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 fetch("https://api.punkapi.com/v2/beers")
   .then((res) => res.json())
-  .then((data) => vis(data));
+  .then(vis);
 
 const temp = document.querySelector("template").content;
-const gode = document.querySelector("#gode");
-const onde = document.querySelector("#onde");
+const parent = document.querySelector("section");
 
 function vis(data) {
   data.forEach((beer) => {
     const klon = temp.cloneNode(true);
     klon.querySelector("h3").textContent = beer.name;
     klon.querySelector("img").src = beer.image_url;
-    if (beer.method.twist) {
-      klon.querySelector("article").classList.add("twisted");
-      klon.querySelector("details span").textContent = beer.method.twist;
-    }
-    if (beer.srm > 20) {
-      dark.appendChild(klon);
-    } else {
-      light.appendChild(klon);
-    }
+    klon.querySelector("details span").textContent = beer.method.twist;
+    klon.querySelector("p span").textContent = beer.abv;
+    klon.querySelector("p+p span").textContent = beer.ebc;
+    parent.appendChild(klon);
   });
 }
