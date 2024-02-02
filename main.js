3bce76f180642c151a1b22a3f2e2af65ca1d1610
Alan <ale@kea.dk>
2024-02-02 10:40:48 +0100

    kun stærk

--- main.js
+++ main.js
@@ -7,13 +7,10 @@ const parent = document.querySelector("section");
 
 function vis(data) {
   data.forEach((beer) => {
+    console.log(beer);
     const klon = temp.cloneNode(true);
     klon.querySelector("h3").textContent = beer.name;
     klon.querySelector("img").src = beer.image_url;
-    if (beer.method.twist) {
-      klon.querySelector("article").classList.add("twisted");
-      klon.querySelector("details span").textContent = beer.method.twist;
-    }
     if (beer.abv > 5.5) {
       klon.querySelector("article").classList.add("strong");
     }
