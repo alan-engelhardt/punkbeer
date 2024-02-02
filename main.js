16dce90dc9e0e448af6227bb9d1623a3a48c74e9
Alan <ale@kea.dk>
2024-02-02 04:45:01 +0100

    simpel gem

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function vis(data) {
     klon.querySelector("h3").textContent = beer.name;
     klon.querySelector("img").src = beer.image_url;
     if (beer.method.twist) {
-      klon.querySelector("article").classList.add("twisted");
+      klon.querySelector("details").classList.remove("gem");
       klon.querySelector("details span").textContent = beer.method.twist;
     }
     klon.querySelector("p span").textContent = beer.abv;
