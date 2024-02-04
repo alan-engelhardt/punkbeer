0048cf87eb503eb1f3f3f27447191db9783900bc
Alan <ale@kea.dk>
2024-02-04 10:44:20 +0100

    pil bund

--- single.js
+++ single.js
@@ -8,7 +8,7 @@ fetch(`https://api.punkapi.com/v2/beers/${id}`)
 function vis(beer) {
   console.log(beer[0].ingredients);
   document.querySelector("h2").textContent = beer[0].name;
-  document.querySelector("img").src = beer[0].image_url;
+  document.querySelector("section+img").src = beer[0].image_url;
   document.querySelector("p").textContent = beer[0].description;
   beer[0].ingredients.hops.forEach((hops) => {
     document.querySelector("#urter").innerHTML += `<li>${hops.name}</li>`;
